6fd00def16c8edecdc9bc30513712d4ad7ffb2e9
unknown <particular6@proton.me>
2024-05-29 15:53:34 +0500

    users now have a password (lol), added password hashing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
 import { Prisma } from '@prisma/client';
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class UsersService {
   async create(createUserDto: Prisma.UserCreateInput) {
     const prisma = new PrismaClient();
+    const salt = 10;
+    const pass = createUserDto.password;
+    const hash = await bcrypt.hash(pass, salt);
+    console.log(hash);
+    createUserDto.password = hash;
+
     const user = await prisma.user.create({
       data: createUserDto
     });
